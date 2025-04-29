16ec6dacbfa28bce232d93111ede10de0017f3c0
RomeroIsHere <guillermoromecu@gmail.com>
2025-04-28 21:06:09 -0600

    Mac Address Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-
-void plot(int x, int y, int h, int w);
-void menu9();
-
-void setup() {
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-void menu9(){
-  int margin=10;
-  int x=80;
-  for(int ii=0;ii<9;ii++){
-    plot((ii/3*x)+margin,(ii%3*x)+margin,x-2*margin,x-2*margin);
+void setup(){
+  Serial.begin(115200);
+  
+  // Variable to store the MAC address
+  uint8_t baseMac[6];
+  
+  // Get MAC address of the WiFi station interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  Serial.print("Station MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
   }
-  for(int ii=0;ii<3;ii++){
-    plot((ii*x)+margin,(3*x)+margin,x-2*margin,x-2*margin);
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Wi-Fi AP interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_SOFTAP);
+  Serial.print("SoftAP MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
   }
-}
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Bluetooth interface
+  esp_read_mac(baseMac, ESP_MAC_BT);
+  Serial.print("Bluetooth MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
 
-void plot(int x, int y, int h, int w){
-//NO OPerator
+  // Get the MAC address of the Ethernet interface
+  esp_read_mac(baseMac, ESP_MAC_ETH);
+  Serial.print("Ethernet MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+}
+ 
+void loop(){
 }
