52c939bbe93615363fbe7b4dc6e2b8970820e02c
RomeroIsHere <guillermoromecu@gmail.com>
2025-04-28 18:53:11 -0600

    Button Design Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
 
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+
+void plot(int x, int y, int h, int w);
+void menu9();
+
 void setup() {
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void menu9(){
+  int margin=10;
+  int x=80;
+  for(int ii=0;ii<9;ii++){
+    plot((ii/3*x)+margin,(ii%3*x)+margin,x-2*margin,x-2*margin);
+  }
+  for(int ii=0;ii<3;ii++){
+    plot((ii*x)+margin,(3*x)+margin,x-2*margin,x-2*margin);
+  }
+}
+
+void plot(int x, int y, int h, int w){
+//NO OPerator
 }
